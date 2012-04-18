7c2dda12f599ce677874cfcb10fd41e94fc09974
Lukasz Szpakowski <luckboy@vp.pl>
2012-04-18 18:24:22 +0200

    Implemented methods in class of extension for board class.

--- src/pl/luckboy/liftchess/ImplicitBoard.scala
+++ src/pl/luckboy/liftchess/ImplicitBoard.scala
@@ -7,24 +7,32 @@ import pl.luckboy.liftchess.engine._
  */
 class ImplicitBoard private(bd: Board)
 {
-  /** The legal moves for board. */
-  def legalMoves: Set[Move] = throw new Exception
+  /** Returns legal moves for board. */
+  def legalMoves: Set[Move] = 
+    pseudoLegalMoves.filter { bd.clone().unsafeFoldSuccessor(_)(false) { (_, _) => true } }
   
-  /** The pseudo legal moves for board. */
-  def pseudoLegalMoves: Set[Move] = throw new Exception
+  /** Returns pseudo legal moves for board. */
+  def pseudoLegalMoves: Set[Move] = {
+    val mvStack = new MoveStack(1, 256)
+    mvStack.generatePseudoLegalMovesWithPopMoves(bd) { (0 until mvStack.size).map(mvStack.move) }.toSet
+  }
   
   /** Returns successor for board if specified move is legal, None otherwise. Successor is copy of board after current side 
    * made move.
    * @param move		the move.
    * @return			the successor.
    */
-  def successor(move: Move): Option[Board] = throw new Exception
+  def successor(move: Move): Option[Board] = {
+    val newBd = bd.clone()
+    if(new ImplicitBoard(newBd).makeMove(move)) Some(newBd) else None
+  }
   
   /** Makes move if specified move is legal. 
    * @param move		the move.
    * @return			true if there made move, false otherwise.
    */
-  def makeMove(move: Move): Boolean = throw new Exception
+  def makeMove(move: Move): Boolean =
+    pseudoLegalMoves.find(move == _).map { _ => bd.unsafeMakeMove(move).isDefined }.getOrElse(false)
 }
 
 /**
