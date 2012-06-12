13cc7d6bf20421f34c21b1ffe4039f14a529e427
Lukasz Szpakowski <luckboy@vp.pl>
2012-06-12 23:36:05 +0200

    Added copyright header to all files.

--- src/pl/luckboy/liftchess/engine/impl/eval/SimpleEvaluator.scala
+++ src/pl/luckboy/liftchess/engine/impl/eval/SimpleEvaluator.scala
@@ -1,3 +1,20 @@
+/*******************************************************************************
+ * Copyright (C) 2012 Łukasz Szpakowski.
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ ******************************************************************************/
+
 package pl.luckboy.liftchess.engine.impl.eval
 import pl.luckboy.liftchess.engine.Board
 import pl.luckboy.liftchess.engine.Evaluator
